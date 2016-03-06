aa891d59ef2898803385f057cdecac3a6c13ed29
Christian Iversen <ci@iversenit.dk>
2016-03-06 18:17:15 +0100

    + Implemented RayTarget trait for Sphere

--- src/sphere.rs
+++ src/sphere.rs
@@ -1,9 +1,13 @@
 #![allow(dead_code)]
 
+use num::Float;
+
+use scene::*;
+use vector;
 use vector::Vector;
 use color::Color;
 use light::Light;
-use num::Float;
+use ray::Ray;
 
 struct Sphere<F: Float>
 {
@@ -21,5 +25,60 @@ impl<F: Float> Sphere<F>
         let light_color = light.color * self.color;
         let reflection_coeff = F::max(normal.cos_angle(m), (normal * (-F::one())).cos_angle(m));
         light_color * reflection_coeff
+    }
+}
+
+impl<F: Float> RayTarget<F> for Sphere<F>
+{
+    fn ray_hit(&self, ray: Ray<F>) -> Option<Vector<F>>
+    {
+        let xd = ray.pos.x - self.pos.x;
+        let yd = ray.pos.y - self.pos.y;
+        let zd = ray.pos.z - self.pos.z;
+        let _0 = F::zero();
+        let _2 = F::from(2.0).unwrap();
+        let _4 = F::from(4.0).unwrap();
+
+        let a =
+            ray.dir.x * ray.dir.x +
+            ray.dir.y * ray.dir.y +
+            ray.dir.z * ray.dir.z;
+
+        let b =
+            _2 *
+            (ray.dir.x * xd +
+             ray.dir.y * yd +
+             ray.dir.z * zd);
+        let c =
+            xd * xd +
+            yd * yd +
+            zd * zd -
+            self.radius * self.radius;
+
+        let d = b * b - _4 * a * c;
+
+        if d < F::zero()
+        {
+            return None;
+        }
+
+        let twice_a = _2 * a;
+        let t1 = (-b + d.sqrt()) / twice_a;
+        let t2 = (-b - d.sqrt()) / twice_a;
+
+        let t = match t1
+        {
+            _ if t1 < _0 => t2,
+            _ if t2 < _0 => t1,
+            _ => F::min(t1, t2),
+        };
+
+        if t < F::from(vector::EPSILON).unwrap()
+        {
+            None
+        } else
+        {
+            Some(ray.extend(t))
+        }
     }
 }
