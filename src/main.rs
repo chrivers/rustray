b9750bd8684a5aaebd4109910f520d68ac4106ae
Christian Iversen <ci@iversenit.dk>
2016-03-06 17:54:39 +0100

    + Added scene, sphere, light pub mods

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,9 @@ pub mod vector;
 pub mod point;
 pub mod camera;
 pub mod ray;
+pub mod scene;
+pub mod sphere;
+pub mod light;
 
 fn main() {
     let buffer = File::create("output.png").unwrap();
