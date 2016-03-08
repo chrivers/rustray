fe62a2d75e831d405a8fd0b1467a69b4070bb1c8
Christian Iversen <ci@iversenit.dk>
2016-03-08 17:19:58 +0100

    + Implemented Float::epsilon

--- src/traits.rs
+++ src/traits.rs
@@ -2,7 +2,19 @@ use num;
 use std::fmt::Debug;
 use std::fmt::Display;
 
-pub trait Float : num::Float + Debug + Display { }
+pub trait Float : num::Float + Debug + Display
+{
+    fn epsilon() -> Self;
+}
 
-impl Float for f32 { }
-impl Float for f64 { }
+impl Float for f32
+{
+    #[inline(always)]
+    fn epsilon() -> Self { 1e-10 }
+}
+
+impl Float for f64
+{
+    #[inline(always)]
+    fn epsilon() -> Self { 1e-10 }
+}
