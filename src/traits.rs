29e604fede06c5e40332cc2b15d0a8d287bc34b5
Christian Iversen <ci@iversenit.dk>
2016-03-08 17:02:43 +0100

    + Implemented Display trait for Float

--- src/traits.rs
+++ src/traits.rs
@@ -1,7 +1,8 @@
 use num;
 use std::fmt::Debug;
+use std::fmt::Display;
 
-pub trait Float : num::Float + Debug { }
+pub trait Float : num::Float + Debug + Display { }
 
 impl Float for f32 { }
 impl Float for f64 { }
