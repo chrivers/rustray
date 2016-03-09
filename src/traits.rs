7aad30434ee8913c511882727692ae776bc28b64
Christian Iversen <ci@iversenit.dk>
2016-03-10 00:48:07 +0100

    + Implemented from_int() and from_float() on Float

--- src/traits.rs
+++ src/traits.rs
@@ -6,6 +6,8 @@ pub trait Float : num::Float + Debug + Display
 {
     fn epsilon() -> Self;
     fn small_value() -> Self;
+    fn from_int(value: u32) -> Self;
+    fn from_float(value: f32) -> Self;
 }
 
 impl Float for f32
@@ -15,6 +17,12 @@ impl Float for f32
 
     #[inline(always)]
     fn small_value() -> Self { 1e-4 }
+
+    #[inline(always)]
+    fn from_int(value: u32) -> Self { value as Self }
+
+    #[inline(always)]
+    fn from_float(value: f32) -> Self { value as Self }
 }
 
 impl Float for f64
@@ -24,4 +32,10 @@ impl Float for f64
 
     #[inline(always)]
     fn small_value() -> Self { 1e-10 }
+
+    #[inline(always)]
+    fn from_int(value: u32) -> Self { value as Self }
+
+    #[inline(always)]
+    fn from_float(value: f32) -> Self { value as Self }
 }
