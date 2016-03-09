b762442f58954277330d8d19b30f3f319827bbbd
Christian Iversen <ci@iversenit.dk>
2016-03-09 01:38:38 +0100

    + Implemented Float::small_value() for delta calculations

--- src/traits.rs
+++ src/traits.rs
@@ -5,16 +5,23 @@ use std::fmt::Display;
 pub trait Float : num::Float + Debug + Display
 {
     fn epsilon() -> Self;
+    fn small_value() -> Self;
 }
 
 impl Float for f32
 {
     #[inline(always)]
     fn epsilon() -> Self { 1e-10 }
+
+    #[inline(always)]
+    fn small_value() -> Self { 1e-4 }
 }
 
 impl Float for f64
 {
     #[inline(always)]
     fn epsilon() -> Self { 1e-10 }
+
+    #[inline(always)]
+    fn small_value() -> Self { 1e-10 }
 }
