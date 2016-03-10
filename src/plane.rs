a68d24b6975d7022c584880ebc37a22fdb59ccde
Christian Iversen <ci@iversenit.dk>
2016-03-10 02:27:04 +0100

    * Moved Plane::ray_hit() into ray_hit_plane() function

--- src/plane.rs
+++ src/plane.rs
@@ -30,22 +30,7 @@ impl<F: Float> RayTarget<F> for Plane<F>
 
     fn ray_hit(&self, ray: &Ray<F>) -> Option<Vector<F>>
     {
-        let v1 = self.pos;
-        let v2 = v1 - self.dir1;
-        let v3 = v1 - self.dir2;
-        let a = (v2.y - v1.y) * (v3.z - v1.z) - (v3.y - v1.y) * (v2.z - v1.z);
-	let b = (v2.z - v1.z) * (v3.x - v1.x) - (v3.z - v1.z) * (v2.x - v1.x);
-	let c = (v2.x - v1.x) * (v3.y - v1.y) - (v3.x - v1.x) * (v2.y - v1.y);
-	let d = (-a * v1.x) + (-b * v1.y) + (-c * v1.z);
-	let t = -(a * ray.pos.x + b * ray.pos.y + c * ray.pos.z + d) / (a * ray.dir.x + b * ray.dir.y + c * ray.dir.z);
-	
-	if t < F::epsilon()
-        {
-            None
-        } else
-        {
-	    Some(ray.extend(t))
-        }
+        ray_hit_plane(&self.pos, &self.dir1, &self.dir2, ray)
     }
 
 }
@@ -55,5 +40,26 @@ impl<F: Float> Plane<F>
     pub fn new(pos: Vector<F>, dir1: Vector<F>, dir2: Vector<F>, color: Color<F>) -> Plane<F>
     {
         Plane { pos: pos, dir1: dir1, dir2: dir2, color: color }
+    }
+}
+
+pub fn ray_hit_plane<F>(pos: &Vector<F>, dir1: &Vector<F>, dir2: &Vector<F>, ray: &Ray<F>) -> Option<Vector<F>>
+    where F: Float
+{
+    let v1 = *pos;
+    let v2 = v1 - *dir1;
+    let v3 = v1 - *dir2;
+    let a = (v2.y - v1.y) * (v3.z - v1.z) - (v3.y - v1.y) * (v2.z - v1.z);
+    let b = (v2.z - v1.z) * (v3.x - v1.x) - (v3.z - v1.z) * (v2.x - v1.x);
+    let c = (v2.x - v1.x) * (v3.y - v1.y) - (v3.x - v1.x) * (v2.y - v1.y);
+    let d = (-a * v1.x) + (-b * v1.y) + (-c * v1.z);
+    let t = -(a * ray.pos.x + b * ray.pos.y + c * ray.pos.z + d) / (a * ray.dir.x + b * ray.dir.y + c * ray.dir.z);
+
+    if t < F::epsilon()
+    {
+        None
+    } else
+    {
+        Some(ray.extend(t))
     }
 }
