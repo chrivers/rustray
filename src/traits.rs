d9df4cd9d653610ff4a8428c67d585b4bdb39a3a
Christian Iversen <ci@iversenit.dk>
2020-08-12 09:52:52 +0200

    + Added HALF const for Float traits

--- src/traits.rs
+++ src/traits.rs
@@ -5,6 +5,7 @@ use std::fmt::Display;
 pub trait Float : num::Float + Debug + Display
 {
     const BIAS: Self;
+    const HALF: Self;
     fn from_i32(value: i32) -> Self;
     fn from_u32(value: u32) -> Self;
     fn from_float(value: f32) -> Self;
@@ -13,6 +14,7 @@ pub trait Float : num::Float + Debug + Display
 impl Float for f32
 {
     const BIAS: Self = 1e-4;
+    const HALF: Self = 0.5;
 
     #[inline(always)]
     fn from_i32(value: i32) -> Self { value as Self }
@@ -27,6 +29,7 @@ impl Float for f32
 impl Float for f64
 {
     const BIAS: Self = 1e-10;
+    const HALF: Self = 0.5;
 
     #[inline(always)]
     fn from_i32(value: i32) -> Self { value as Self }
