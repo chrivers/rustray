fc956d9919441164e220ae9d86948f5dbf73e62d
Christian Iversen <ci@iversenit.dk>
2020-08-12 09:37:38 +0200

    + Implemented .parametric() configurator for Camera

--- src/camera.rs
+++ src/camera.rs
@@ -29,6 +29,35 @@ impl<F: Float> Camera<F>
         Camera { pos, dir, hor, ver }
     }
 
+    pub fn parametric(
+        pos: Vector<F>,
+        lookat: Vector<F>,
+        fov: F,
+        xres: usize,
+        yres: usize
+    ) -> Camera<F>
+    {
+        let dir = (lookat - pos).normalized();
+        let u = dir.crossed(Vector::new(F::zero(), F::one(), F::zero()));
+        let v = u.crossed(dir);
+        let u = u.normalized();
+        let v = v.normalized();
+        let aspect_ratio = F::from_u32(yres as u32) / F::from_u32(xres as u32);
+        let viewplane_half_width = (fov / F::from_u32(2)).tan();
+        let viewplane_half_height = aspect_ratio * viewplane_half_width;
+        let viewplane_bottom_left = lookat - (v * viewplane_half_height) - (u * viewplane_half_width);
+        let x_inc_vector = (u * F::from_u32(2) * viewplane_half_width)  / F::from_u32(xres as u32);
+        let y_inc_vector = (v * F::from_u32(2) * viewplane_half_height) / F::from_u32(yres as u32);
+        info!("aspect_ratio: {}", aspect_ratio);
+        info!("vp_half_width: {}", viewplane_half_width);
+        info!("vp_half_height: {}", viewplane_half_height);
+        info!("vp_bottom_left: {:?}", viewplane_bottom_left);
+        info!("x_int_vector: {:?}", x_inc_vector);
+        info!("y_int_vector: {:?}", y_inc_vector);
+
+        Camera::raw(pos, viewplane_bottom_left, x_inc_vector, y_inc_vector)
+    }
+
     pub fn get_ray(self, point: Point<F>) -> Ray<F>
     {
         let one_half = F::from_float(0.5);
