b2e1e6b32b8804d61c2b7d21d1d920eb68f45dd4
Christian Iversen <ci@iversenit.dk>
2021-09-10 14:26:48 +0200

    * Vastly simplified Plane::ray_hit_plane() by using vector helpers

--- src/plane.rs
+++ src/plane.rs
@@ -44,14 +44,9 @@ impl<F: Float> Plane<F>
 pub fn ray_hit_plane<F>(pos: &Vector<F>, dir1: &Vector<F>, dir2: &Vector<F>, ray: &Ray<F>) -> Option<Vector<F>>
     where F: Float
 {
-    let v1 = *pos;
-    let v2 = v1 - *dir1;
-    let v3 = v1 - *dir2;
-    let a = (v2.y - v1.y) * (v3.z - v1.z) - (v3.y - v1.y) * (v2.z - v1.z);
-    let b = (v2.z - v1.z) * (v3.x - v1.x) - (v3.z - v1.z) * (v2.x - v1.x);
-    let c = (v2.x - v1.x) * (v3.y - v1.y) - (v3.x - v1.x) * (v2.y - v1.y);
-    let d = (-a * v1.x) + (-b * v1.y) + (-c * v1.z);
-    let t = -(a * ray.pos.x + b * ray.pos.y + c * ray.pos.z + d) / (a * ray.dir.x + b * ray.dir.y + c * ray.dir.z);
+    let abc = dir1.crossed(*dir2);
+    let d = abc.dot(*pos);
+    let t = (-abc.dot(ray.pos) + d) / abc.dot(ray.dir);
 
     if t < F::epsilon() {
         None
