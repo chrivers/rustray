73111750f579f26ef6048ecdfc0d31845df6e52c
Christian Iversen <ci@iversenit.dk>
2021-09-14 21:47:13 +0200

    + Added {struct Hit} to ray.rs. This represents a ray hit on a RayTarget.

--- src/ray.rs
+++ src/ray.rs
@@ -1,11 +1,22 @@
 use crate::traits::Float;
 use crate::vector::Vector;
+use crate::point::Point;
+use crate::scene::RayTarget;
 
 #[derive(Clone, Copy, Debug)]
 pub struct Ray<F: Float>
 {
     pub pos: Vector<F>,
     pub dir: Vector<F>,
+}
+
+pub struct Hit<'a, F: Float>
+{
+    pub pos: Vector<F>,
+    pub dir: Vector<F>,
+    pub normal: Vector<F>,
+    pub uv: Point<F>,
+    pub obj: &'a dyn RayTarget<F>,
 }
 
 impl<F: Float> Ray<F>
@@ -23,5 +34,10 @@ impl<F: Float> Ray<F>
     pub fn extend(self, scale: F) -> Vector<F>
     {
         self.pos + self.dir * scale
+    }
+
+    pub fn hit_at(self, ext: F, normal: Vector<F>, uv: Point<F>, obj: &dyn RayTarget<F>) -> Hit<'_, F>
+    {
+        Hit { pos: self.extend(ext), dir: self.dir, normal, uv, obj }
     }
 }
