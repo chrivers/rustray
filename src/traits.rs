5bd7ef5c8f544c0a8c3cdeff16f33a6f7e5a047a
Christian Iversen <ci@iversenit.dk>
2021-09-14 18:13:04 +0200

    + Added constant Float::TWO

--- src/traits.rs
+++ src/traits.rs
@@ -5,6 +5,7 @@ pub trait Float : num::Float + num::Signed + Debug + Display + Sync
 {
     const BIAS: Self;
     const HALF: Self;
+    const TWO:  Self;
     fn from_i32(value: i32) -> Self;
     fn from_u32(value: u32) -> Self;
     fn from_float(value: f32) -> Self;
@@ -15,6 +16,7 @@ impl Float for f32
 {
     const BIAS: Self = 1e-4;
     const HALF: Self = 0.5;
+    const TWO:  Self = 2.0;
 
     #[inline(always)]
     fn from_i32(value: i32) -> Self { value as Self }
@@ -30,6 +32,7 @@ impl Float for f64
 {
     const BIAS: Self = 1e-10;
     const HALF: Self = 0.5;
+    const TWO:  Self = 2.0;
 
     #[inline(always)]
     fn from_i32(value: i32) -> Self { value as Self }
