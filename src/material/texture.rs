64801528a1890554aed39c84ccf1d96258b16217
Christian Iversen <ci@iversenit.dk>
2021-09-20 18:02:32 +0200

    * Change material::Texture to refer to Sampler, instead of directly interacting with GenericImageView. This simplifies the code, and allows e.g. bilinear filtering to be enabled for any texture.

--- src/material/texture.rs
+++ src/material/texture.rs
@@ -7,34 +7,26 @@ use image::{Pixel, Rgb};
 use num_traits::ToPrimitive;
 
 #[derive(Copy, Clone)]
-pub struct Texture<F: Float, I: GenericImageView + Sync>
+pub struct Texture<F: Float, S: Sampler<F, Color<F>>>
 {
     _f: PhantomData<F>,
-    img: I
+    img: S
 }
 
-impl<F: Float, I: GenericImageView + Sync> Texture<F, I>
+impl<F: Float, S: Sampler<F, Color<F>>> Texture<F, S>
 {
-    pub fn new(img: I) -> Self
+    pub fn new(img: S) -> Self
     {
         Self { _f: PhantomData::<F> {}, img }
     }
 }
 
-impl<F: Float, I: GenericImageView + Sync> Material for Texture<F, I>
+impl<F: Float, S: Sampler<F, Color<F>>> Material for Texture<F, S>
 {
     type F = F;
 
     fn render(&self, hit: &Hit<F>, maxel: &Maxel<F>, lights: &[Light<F>], rt: &dyn RayTracer<F>, lvl: u32) -> Color<F>
     {
-        let (w, h) = self.img.dimensions();
-        let x: u32 = (maxel.uv.x * F::from_u32(w)).to_u32().unwrap_or(0) % w;
-        let y: u32 = (maxel.uv.y * F::from_u32(h)).to_u32().unwrap_or(0) % h;
-        let Rgb([r, g, b]) = self.img.get_pixel(x, y).to_rgb();
-        Color::new(
-            F::from_f32(r.to_f32().unwrap() / 255.0),
-            F::from_f32(g.to_f32().unwrap() / 255.0),
-            F::from_f32(b.to_f32().unwrap() / 255.0),
-        )
+        self.img.sample(maxel.uv)
     }
 }
