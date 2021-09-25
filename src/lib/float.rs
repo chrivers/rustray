fa8100b3ce4b3609c2a6b169a6e59da50bf31403
Christian Iversen <ci@iversenit.dk>
2021-09-25 15:17:50 +0200

    + Added F::ZERO and F::ONE consts for Float

--- src/lib/float.rs
+++ src/lib/float.rs
@@ -7,7 +7,9 @@ use std::ops::{Add, Sub, Mul};
 pub trait Float : num::Float + NumAssignOps + pow::Pow<Self, Output=Self> + FloatConst + num::Signed + Lerp + Debug + Display + Sync
 {
     const BIAS: Self;
+    const ZERO: Self;
     const HALF: Self;
+    const ONE:  Self;
     const TWO:  Self;
     const FOUR: Self;
     fn from_i32(value: i32) -> Self;
@@ -44,7 +46,9 @@ impl Lerp for f64 {
 impl Float for f32
 {
     const BIAS: Self = 1e-4;
+    const ZERO: Self = 0.0;
     const HALF: Self = 0.5;
+    const ONE:  Self = 1.0;
     const TWO:  Self = 2.0;
     const FOUR: Self = 4.0;
 
@@ -64,7 +68,9 @@ impl Float for f32
 impl Float for f64
 {
     const BIAS: Self = 1e-10;
+    const ZERO: Self = 0.0;
     const HALF: Self = 0.5;
+    const ONE:  Self = 1.0;
     const TWO:  Self = 2.0;
     const FOUR: Self = 4.0;
 
