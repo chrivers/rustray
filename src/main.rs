291aaeec47189adb39be19d8fc2a56ed9265bcd3
Christian Iversen <ci@iversenit.dk>
2021-09-26 11:07:00 +0200

    - Remove old dead code

--- src/main.rs
+++ src/main.rs
@@ -187,45 +187,4 @@ fn main() {
     let buffer = File::create("output.png").unwrap();
     let png = PngEncoder::new(buffer);
     png.encode(&img.into_raw(), WIDTH as u32, HEIGHT as u32, ColorType::Rgb8).expect("Failed to encode");
-
-    //Construct a new ImageBuffer with the specified width and height.
-
-    // //Construct a new by repeated calls to the supplied closure.
-    // let img = ImageBuffer::from_fn(512, 512, |x, y| {
-    //     let dx: i32 = x as i32 - 256;
-    //     let dy: i32 = y as i32 - 256;
-    //     let n = ((dx*dx + dy*dy*4) as f32).sqrt() as i32;
-    //     match n
-    //     {
-    //         _ if n < 0 => image::Rgb([255, 255, 255]),
-    //         _ if n > 255 => image::Rgb([0, 0, 0]),
-    //         _ => {
-    //             let n = 255 - n as u8;
-    //             image::Rgb([n, n, n])
-    //         }
-    //     }
-    //     // if x % 2 == 0 {
-    //     //     image::Luma([(y % 128) as u8])
-    //     // } else {
-    //     //     image::Luma([255u8])
-    //     // }
-    // });
-
-    // png.encode(&img.into_raw(), img.width(), img.height(), ColorType::RGB(8)).expect("Failed to encode");
-    // Change this to OpenGL::V2_1 if not working.
-    // let opengl = OpenGL::V3_2;
-    // let window: PistonWindow =
-    //     WindowSettings::new("Hello Piston!", [640, 480])
-    //     .opengl(opengl)
-    //     .exit_on_esc(true)
-    //     .build()
-    //     .unwrap();
-    // for e in window {
-    //     e.draw_2d(|c, g| {
-    //         clear([1.0; 4], g);
-    //         rectangle([1.0, 0.0, 0.0, 1.0], // red
-    //                   [0.0, 0.0, 100.0, 100.0],
-    //                   c.transform, g);
-    //     });
-    // }
 }
