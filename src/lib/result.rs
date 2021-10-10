53351c473f7dba6eb4b4dc9f7ec93b7a461e1322
Christian Iversen <ci@iversenit.dk>
2021-10-10 22:25:18 +0200

    + Extended result type to handle pest error and parse errors

--- src/lib/result.rs
+++ src/lib/result.rs
@@ -10,8 +10,14 @@ pub enum Error
     ImageError(#[from] image::ImageError),
 
     #[error(transparent)]
+    PestError(#[from] pest::error::Error<crate::format::sbt::Rule>),
+
+    #[error(transparent)]
     ZipError(#[from] zip::result::ZipError),
 
     #[error(transparent)]
     Reqwest(#[from] reqwest::Error),
+
+    #[error("parse error")]
+    ParseError()
 }
