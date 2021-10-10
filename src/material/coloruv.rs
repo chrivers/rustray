32973747b5b24d46002869deee550fb56a47aff9
Christian Iversen <ci@iversenit.dk>
2021-10-10 11:30:15 +0200

    * Make ColorUV use red and blue channel, for improved clarity

--- src/material/coloruv.rs
+++ src/material/coloruv.rs
@@ -21,6 +21,6 @@ impl<F: Float> Material for ColorUV<F>
 
     fn render(&self, _hit: &Hit<F>, maxel: &Maxel<F>, _lights: &[Light<F>], _rt: &dyn RayTracer<F>) -> Color<F>
     {
-        Color::new(self.scale * maxel.uv.x, self.scale * maxel.uv.y, F::zero())
+        Color::new(self.scale * maxel.uv.x, F::ZERO, self.scale * maxel.uv.y)
     }
 }
