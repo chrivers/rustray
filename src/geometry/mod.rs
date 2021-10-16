afef93892419c69e16bce34075d91a938b75fdac
Christian Iversen <ci@iversenit.dk>
2021-10-16 20:44:48 +0200

    + Added build_aabb_* helper functions

--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -56,6 +56,26 @@ where
 {
 }
 
+pub fn build_aabb_ranged<F: Float>(xfrm: &Matrix4<F>, x: [F; 2], y: [F; 2], z: [F; 2]) -> AABB
+{
+    /* Transform all corner points, expand aabb with each result */
+    let mut aabb: AABB = AABB::empty();
+    for px in x {
+        for py in y {
+            for pz in z {
+                let p = vec3!(px, py, pz).xfrm(&xfrm);
+                aabb.grow_mut(&p.into_point3());
+            }
+        }
+    }
+    aabb
+}
+
+pub fn build_aabb_symmetric<F: Float>(xfrm: &Matrix4<F>, x: F, y: F, z: F) -> AABB
+{
+    build_aabb_ranged(xfrm, [-x, x], [-y, y], [-z, z])
+}
+
 macro_rules! aabb_impl_fm {
     ( $t:ty ) =>
     {
@@ -88,6 +108,7 @@ pub(crate) mod geo_util {
     pub use crate::scene::*;
     pub use crate::material::Material;
     pub use super::Geometry;
+    pub use crate::geometry::{build_aabb_ranged, build_aabb_symmetric};
 
     pub use cgmath::{Matrix4, Transform, SquareMatrix};
 
