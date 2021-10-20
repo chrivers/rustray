5bc3060ef59eca5e7a3151e6ca80939fd59e279e
Christian Iversen <ci@iversenit.dk>
2021-10-20 17:45:11 +0200

    * Convert Bumpmap material to use sampler for specular power factor

--- src/material/bumpmap.rs
+++ src/material/bumpmap.rs
@@ -1,35 +1,36 @@
 use super::mat_util::*;
 
 #[derive(Copy, Clone, Debug)]
-pub struct Bumpmap<F: Float, S: Sampler<F, Vector<F>>, M: Material<F=F>>
+pub struct Bumpmap<F: Float, S1: Sampler<F, F>, S2: Sampler<F, Vector<F>>, M: Material<F=F>>
 {
-    pow: F,
-    img: S,
+    pow: S1,
+    img: S2,
     mat: M,
 }
 
-impl<F: Float, S: Sampler<F, Vector<F>>, M: Material<F=F>> Bumpmap<F, S, M>
+impl<F: Float, S1: Sampler<F, F>, S2: Sampler<F, Vector<F>>, M: Material<F=F>> Bumpmap<F, S1, S2, M>
 {
-    pub fn new(pow: F, img: S, mat: M) -> Self
+    pub fn new(pow: S1, img: S2, mat: M) -> Self
     {
         Self { pow, img, mat }
     }
 }
 
-impl<'a, F: Float, S: Sampler<F, Vector<F>>, M: Material<F=F>> Material for Bumpmap<F, S, M>
+impl<'a, F: Float, S1: Sampler<F, F>, S2: Sampler<F, Vector<F>>, M: Material<F=F>> Material for Bumpmap<F, S1, S2, M>
 {
     type F = F;
 
     fn render(&self, hit: &Hit<F>, maxel: &Maxel<F>, lights: &[&dyn Light<F>], rt: &dyn RayTracer<F>) -> Color<F>
     {
         let n = self.img.sample(maxel.uv);
+        let pow = self.pow.sample(maxel.uv);
 
         let mut mxl = *maxel;
 
         let nx =
             mxl.normalu * n.x +
             mxl.normalv * n.y +
-            mxl.normal  * n.z / (self.pow + F::BIAS);
+            mxl.normal  * n.z / (pow + F::BIAS);
 
         mxl.normal = nx.normalize();
 
