0291ae68ffcea2b73491f76d443edd1d6f6fd887
Christian Iversen <ci@iversenit.dk>
2021-10-26 23:46:47 +0200

    + Extended .obj in TriangleMesh to support texture coordinates, texture maps, materials and material properties

--- src/geometry/trianglemesh.rs
+++ src/geometry/trianglemesh.rs
@@ -5,7 +5,6 @@ use std::path::Path;
 
 use obj::Obj;
 use obj::ObjMaterial;
-use obj::ObjData;
 
 use rtbvh::{Bvh, Builder, Bounds};
 use std::num::NonZeroUsize;
@@ -77,8 +76,40 @@ impl<F: Float, M: Material<F=F> + Clone> TriangleMesh<F, M>
             bvh,
         }
     }
+}
 
-    pub fn load_obj(obj: ObjData, pos: Vector<F>, scale: F, mat: M) -> Self
+fn sampler<'a, F: Float + 'a>(val: Option<[f32; 3]>) -> DynSampler<'a, F, Color<F>>
+{
+    match val {
+        None => Color::white().dynsampler(),
+        Some([r, g, b]) => Color::new(
+            F::from_f32(r),
+            F::from_f32(g),
+            F::from_f32(b),
+        ).dynsampler(),
+    }
+}
+
+fn obj_sampler<'a, F: Float + 'a>(resdir: &Path, map: &Option<String>, col: &Option<[f32; 3]>) -> DynSampler<'a, F, Color<F>>
+{
+    match map {
+        Some(ref kd) => {
+            info!("loading [{}]", kd);
+            image::open(resdir.join(kd)).map(|m| m.bilinear().dynsampler()).unwrap_or_else(
+                |_| {
+                    warn!("Missing texture [{}]", kd);
+                    Color::white().dynsampler()
+                }
+            )
+            /* image::open(obj.path.join(kd)).map(|m| Bilinear::new(m).dynsampler()).unwrap_or_else(|_| Color::white().dynsampler()) */
+        }
+        None => sampler(*col),
+    }
+}
+
+impl<F: Float + Texel + 'static> TriangleMesh<F, DynMaterial<'static, F>>
+{
+    pub fn load_obj(mut obj: Obj, pos: Vector<F>, scale: F) -> Self
     {
         let mut corner = Vector::new(
             F::max_value(),
@@ -86,37 +117,85 @@ impl<F: Float, M: Material<F=F> + Clone> TriangleMesh<F, M>
             F::max_value()
         );
 
-        for o in &obj.objects {
+        for o in &obj.data.objects {
             for g in &o.groups {
                 for poly in &g.polys {
                     for n in 0..(poly.0.len() - 1) {
-                        corner.x = corner.x.min(F::from_f32(obj.position[poly.0[n].0][0]));
-                        corner.y = corner.y.min(F::from_f32(obj.position[poly.0[n].0][1]));
-                        corner.z = corner.z.min(F::from_f32(obj.position[poly.0[n].0][2]));
+                        corner.x = corner.x.min(F::from_f32(obj.data.position[poly.0[n].0][0]));
+                        corner.y = corner.y.min(F::from_f32(obj.data.position[poly.0[n].0][1]));
+                        corner.z = corner.z.min(F::from_f32(obj.data.position[poly.0[n].0][2]));
                     }
                 }
             }
         }
 
-        let mut tris: Vec<Triangle<F, M>> = vec![];
-        for o in &obj.objects {
-            for g in &o.groups {
+        let mut tris: Vec<Triangle<F, DynMaterial<F> >> = vec![];
+        obj.load_mtls().unwrap();
+        /* info!("mats: {:#?}", obj.data.material_libs); */
+        for o in &obj.data.objects {
+            for (_i, g) in o.groups.iter().enumerate() {
+                let mat = if let Some(ObjMaterial::Mtl(ref omat)) = g.material {
+                    let ke = obj_sampler(&obj.path, &omat.map_ke, &omat.ke);
+                    let kd = obj_sampler(&obj.path, &omat.map_kd, &omat.kd);
+                    let ks = obj_sampler(&obj.path, &omat.map_ks, &omat.ks);
+                    /* let kt = obj_sampler(&obj.path, &omat.map_kt, &omat.kt); */
+                    /* let kr = obj_sampler(&obj.path, &omat.map_refl, &omat.kr); */
+                    /* let ns = obj_sampler(&obj.path, &omat.map_ns, F::from_f32(omat.ns.unwrap_or(1.0)) */
+
+                    Smart::new(
+                        F::from_f32(omat.ni.unwrap_or(1.0)),
+                        F::from_f32(omat.ns.unwrap_or(1.0)),
+                        ke,
+                        kd,
+                        ks,
+                        Color::black().dynsampler(),
+                        Color::white().dynsampler()
+                    ).dynamic()
+                } else {
+                    Color::white().dynamic()
+                };
+
                 for poly in &g.polys {
+                    if !obj.data.texture.is_empty() && poly.0[0].1.is_none() {
+                        continue
+                    }
                     for n in 1..(poly.0.len() - 1) {
+                        let a = (a2vec(&obj.data.position[poly.0[0  ].0]) - corner) * scale + pos;
+                        let b = (a2vec(&obj.data.position[poly.0[n  ].0]) - corner) * scale + pos;
+                        let c = (a2vec(&obj.data.position[poly.0[n+1].0]) - corner) * scale + pos;
+
                         /* FIXME: .unwrap() is a terrible when loading data from a file */
+                        let (na, nb, nc) = if obj.data.normal.is_empty() {
+                            let n = (a-b).cross(a-c);
+                            (
+                                n, n, n
+                            )
+                        } else {
+                            (
+                                a2vec(&obj.data.normal[poly.0[0].2.unwrap()]).normalize(),
+                                a2vec(&obj.data.normal[poly.0[n].2.unwrap()]).normalize(),
+                                a2vec(&obj.data.normal[poly.0[n+1].2.unwrap()]).normalize(),
+                            )
+                        };
+
+                        let (ta, tb, tc) = if obj.data.texture.is_empty() {
+                            (
+                                Point::zero(),
+                                Point::zero(),
+                                Point::zero(),
+                            )
+                        } else {
+                            (
+                                a2point(&obj.data.texture[poly.0[0].1.unwrap()]),
+                                a2point(&obj.data.texture[poly.0[n].1.unwrap()]),
+                                a2point(&obj.data.texture[poly.0[n+1].1.unwrap()]),
+                            )
+                        };
+
                         let tri = Triangle::new (
-                            (a2vec(&obj.position[poly.0[0  ].0]) - corner) * scale + pos,
-                            (a2vec(&obj.position[poly.0[n  ].0]) - corner) * scale + pos,
-                            (a2vec(&obj.position[poly.0[n+1].0]) - corner) * scale + pos,
-
-                            a2vec(&obj.normal[poly.0[0].2.unwrap()]).normalize(),
-                            a2vec(&obj.normal[poly.0[n].2.unwrap()]).normalize(),
-                            a2vec(&obj.normal[poly.0[n+1].2.unwrap()]).normalize(),
-
-                            a2point(&obj.texture[poly.0[0].1.unwrap()]),
-                            a2point(&obj.texture[poly.0[n].1.unwrap()]),
-                            a2point(&obj.texture[poly.0[n+1].1.unwrap()]),
-
+                            a, b, c,
+                            na, nb, nc,
+                            ta, tb, tc,
                             mat.clone(),
                         );
                         tris.push(tri);
@@ -125,7 +204,7 @@ impl<F: Float, M: Material<F=F> + Clone> TriangleMesh<F, M>
             }
         }
 
-        info!("loaded .obj [index: {}, normal: {}, uv: {}, face: {}]", obj.position.len(), obj.normal.len(), obj.texture.len(), tris.len());
+        info!("loaded .obj [index: {}, normal: {}, uv: {}, face: {}]", obj.data.position.len(), obj.data.normal.len(), obj.data.texture.len(), tris.len());
 
         TriangleMesh::new(tris)
     }
