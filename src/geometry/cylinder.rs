bc714e350c9213d0d75691505928137e4bc87062
Christian Iversen <ci@iversenit.dk>
2021-10-27 18:18:15 +0200

    * Reworked cylinder intersection code to use quadratic2() helper, and move some reused code into inline functions.

--- src/geometry/cylinder.rs
+++ src/geometry/cylinder.rs
@@ -32,65 +32,38 @@ impl<F: Float, M: Material<F=F>> Geometry<F> for Cylinder<F, M>
 
         fn isect_body<F: Float>(r: &Ray<F>, capped: bool) -> Option<(F, Vector<F>)>
         {
-            let x0 = r.pos.x;
-            let y0 = r.pos.y;
-            let x1 = r.dir.x;
-            let y1 = r.dir.y;
+            let p = r.pos;
+            let d = r.dir;
 
-            let a = x1*x1 + y1*y1;
-            let b = F::TWO * (x0*x1 + y0*y1);
-            let c = x0*x0 + y0*y0 - F::ONE;
+            let a = d.x*d.x + d.y*d.y;
+            let b = F::TWO * (p.x*d.x + p.y*d.y);
+            let c = p.x*p.x + p.y*p.y - F::ONE;
 
-            if a == F::ZERO {
-                /* This implies that x1 = 0.0 and y1 = 0.0, which further */
-                /* implies that the ray is aligned with the body of the cylinder, */
-                /* so no intersection. */
+            let (t1, t2) = crate::lib::ray::quadratic2(a, b, c)?;
+            if t1 < F::ZERO || t2 < F::ZERO {
                 return None
             }
 
-            let discriminant = b * b - F::FOUR * a * c;
+            fn isect_side<F: Float>(r: &Ray<F>, t: F, capped: bool) -> Option<(F, Vector<F>)>
+            {
+                let p = r.extend(t);
 
-            if discriminant < F::ZERO {
-                return None
-            }
-
-            let discriminant = discriminant.sqrt();
-
-            let t2 = (-b + discriminant) / (F::TWO * a);
-
-            if t2 <= F::BIAS {
-                return None
-            }
-
-            let t1 = (-b - discriminant) / (F::TWO * a);
-
-            if t1 > F::BIAS {
-                /* Two intersections. */
-                let p = r.extend(t1);
-                let z = p[2];
-                if z >= F::ZERO && z <= F::ONE {
-                    return Some((t1, vec3!(p[0], p[1], F::ZERO)))
+                if p.z < F::ZERO || p.z > F::ONE {
+                    return None
                 }
-            }
 
-            let p = r.extend(t2);
+                let mut normal = vec3!(p.x, p.y, F::ZERO);
 
-            let z = p[2];
-
-            if z >= F::ZERO && z <= F::ONE {
-
-                let mut normal = vec3!(p[0], p[1], F::ZERO);
                 /* In case we are _inside_ the _uncapped_ cone, we need to flip the normal. */
                 /* Essentially, the cone in this case is a double-sided surface */
                 /* and has _2_ normals */
                 if !capped && r.dir.dot(normal) > F::ZERO {
                     normal = -normal
                 }
-
-                return Some((t2, normal))
+                Some((t, normal))
             }
 
-            None
+            isect_side(r, t1, capped).or_else(|| isect_side(r, t2, capped))
         }
 
         fn isect_caps<F: Float>(r: &Ray<F>) -> Option<(F, Vector<F>)>
@@ -99,48 +72,40 @@ impl<F: Float, M: Material<F=F>> Geometry<F> for Cylinder<F, M>
             let dz = r.dir.z;
 
             if dz == F::ZERO {
-                 return None
+                return None
             }
 
             let t1;
             let t2;
 
             if dz > F::ZERO {
-                 t1 = (-pz)/dz;
-                 t2 = (F::ONE-pz)/dz;
+                t1 = (-pz)/dz;
+                t2 = (F::ONE-pz)/dz;
             } else {
-                 t1 = (F::ONE-pz)/dz;
-                 t2 = (-pz)/dz;
+                t1 = (F::ONE-pz)/dz;
+                t2 = (-pz)/dz;
             }
 
-            if t2 < F::BIAS {
+            if t1 < F::BIAS {
                 return None
             }
 
-            if t1 >= F::BIAS {
-                let p = r.extend(t1);
-                if (p[0]*p[0] + p[1]*p[1]) <= F::ONE {
-                    let n = if dz > F::ZERO {
-                        /* Intersection with cap at z = 0. */
-                        -Vector::unit_z()
-                    } else {
-                        Vector::unit_z()
-                    };
-                    return Some((t1, n))
-                }
+            let t = if t1 >= F::BIAS {
+                t1
             } else {
-                let p = r.extend(t2);
-                if (p[0]*p[0] + p[1]*p[1]) <= F::ONE {
-                    let n = if dz > F::ZERO {
-                        /* Intersection with interior of cap at z = 1. */
-                        Vector::unit_z()
-                    } else {
-                        -Vector::unit_z()
-                    };
-                    return Some((t2, n))
-                }
-            }
+                t2
+            };
 
+            let p = r.extend(t);
+            if (p.x * p.x + p.y * p.y) <= F::ONE {
+                let n = if dz > F::ZERO {
+                    /* Intersection with cap at z = 0. */
+                    -Vector::unit_z()
+                } else {
+                    Vector::unit_z()
+                };
+                return Some((t, n))
+            }
             None
         }
 
