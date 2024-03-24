30085489feb00c3f8887a2acc36864ca27b91084
Christian Iversen <ci@iversenit.dk>
2024-03-24 14:28:41 +0100

    * Refactor rayon support out into optional "rayon" feature (enabled by default)

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,18 @@ use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
+#[cfg(not(feature = "rayon"))]
+use indicatif::ProgressIterator;
+
+#[cfg(feature = "rayon")]
 use indicatif::ParallelProgressIterator;
+#[cfg(feature = "rayon")]
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 use image::{ColorType, ImageBuffer, Rgb};
 
 use log::LevelFilter;
 use pest::Parser;
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 use rustray::format::sbt2::{Rule as Rule2, SbtBuilder, SbtParser2};
 use rustray::geometry::{FiniteGeometry, Geometry};
@@ -147,8 +152,13 @@ where
 
     let camera = &tracer.scene().cameras[0];
 
-    let lines: Vec<_> = (0..height)
-        .into_par_iter()
+    #[cfg(feature = "rayon")]
+    let indices = (0..height).into_par_iter();
+
+    #[cfg(not(feature = "rayon"))]
+    let indices = (0..height).into_iter();
+
+    let lines: Vec<_> = indices
         .progress_with(pb)
         .map(|y| tracer.generate_span(camera, y))
         .collect();
