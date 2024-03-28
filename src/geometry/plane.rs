704fed0f1d1f0604fcb9ccd926f6ca8b055c1c07
Christian Iversen <ci@iversenit.dk>
2024-03-28 17:15:46 +0100

    + Added ui implementation for Plane

--- src/geometry/plane.rs
+++ src/geometry/plane.rs
@@ -11,6 +11,33 @@ pub struct Plane<F: Float, M: Material<F = F>> {
     mat: M,
 }
 
+impl<F: Float, M: Material<F = F>> Interactive for Plane<F, M> {
+    #[cfg(feature = "gui")]
+    fn ui(&mut self, ui: &mut egui::Ui) {
+        egui::Grid::new("grid")
+            .num_columns(2)
+            .spacing([40.0, 4.0])
+            .striped(true)
+            .show(ui, |ui| {
+                position_ui(ui, &mut self.pos, "Position");
+                self.mat.ui(ui);
+            });
+    }
+}
+
+impl<F: Float, M: Material<F = F>> SceneObject for Plane<F, M> {
+    fn get_name(&self) -> &str {
+        "Plane"
+    }
+
+    fn get_interactive(&mut self) -> Option<&mut dyn Interactive> {
+        Some(self)
+    }
+    fn get_id(&self) -> Option<usize> {
+        Some(std::ptr::addr_of!(*self) as usize)
+    }
+}
+
 impl<F: Float, M: Material<F = F>> Geometry<F> for Plane<F, M> {
     fn uv(&self, maxel: &mut Maxel<F>) -> Point<F> {
         let u = self.u.dot(maxel.pos);
