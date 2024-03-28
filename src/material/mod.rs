c116e37ca2faf869f91b56f954c72793ae46f91f
Christian Iversen <ci@iversenit.dk>
2024-03-28 17:17:44 +0100

    + Added basic ui implementation for common Material code

--- src/material/mod.rs
+++ src/material/mod.rs
@@ -38,12 +38,27 @@ impl<F: Float> Material for Color<F> {
     fn render(&self, _maxel: &mut Maxel<F>, _rt: &dyn RayTracer<F>) -> Color<F> {
         *self
     }
+
+    #[cfg(feature = "gui")]
+    fn ui(&mut self, ui: &mut egui::Ui) {
+        crate::frontend::gui::color_ui(ui, self, "Color");
+    }
 }
 
 impl<F: Float> Material for Arc<Box<dyn Material<F = F>>> {
     type F = F;
     fn render(&self, maxel: &mut Maxel<F>, rt: &dyn RayTracer<F>) -> Color<F> {
         self.as_ref().render(maxel, rt)
+    }
+
+    #[cfg(feature = "gui")]
+    fn ui(&mut self, ui: &mut egui::Ui) {
+        match Arc::<Box<_>>::get_mut(self) {
+            Some(mat) => mat.ui(ui),
+            None => {
+                ui.label("nope :(");
+            }
+        }
     }
 }
 
@@ -53,11 +68,16 @@ pub(crate) mod mat_util {
     pub use crate::material::{DynMaterial, Material};
     pub use crate::sampler::Sampler;
     pub use crate::sampler::Texel;
-    pub use crate::scene::{Light, RayTracer};
+    pub use crate::scene::{Interactive, Light, RayTracer};
     pub use crate::types::ray::{Maxel, Ray};
     pub use crate::types::vector::{InnerSpace, Vectorx};
     pub use crate::types::{Color, Float, Point, Vector};
     pub use crate::{point, vec3};
+
+    #[cfg(feature = "gui")]
+    pub use crate::frontend::gui::color_ui;
+    #[cfg(feature = "gui")]
+    pub use egui::{CollapsingHeader, Slider};
 
     pub use cgmath::VectorSpace;
 
