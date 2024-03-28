add8c3cfd3cfbfe55d62abbbe73087e1072eb46f
Christian Iversen <ci@iversenit.dk>
2024-03-28 21:12:28 +0100

    + Enable all(?) optional clippy lints, and suppress specific ones that have too many false negatives

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,50 @@
 #![feature(box_patterns)]
 #![feature(const_trait_impl)]
 #![feature(effects)]
+#![warn(
+    clippy::all,
+    clippy::correctness,
+    clippy::pedantic,
+    clippy::cargo,
+    clippy::nursery,
+    clippy::perf,
+    clippy::style
+)]
+#![allow(
+    clippy::cargo_common_metadata,
+    clippy::cast_lossless,
+    clippy::cast_possible_truncation,
+    clippy::cast_possible_wrap,
+    clippy::cast_precision_loss,
+    clippy::cast_sign_loss,
+    clippy::if_not_else,
+    clippy::inline_always,
+    clippy::items_after_statements,
+    clippy::many_single_char_names,
+    clippy::map_unwrap_or,
+    clippy::mismatching_type_param_order,
+    clippy::missing_errors_doc,
+    clippy::missing_panics_doc,
+    clippy::module_name_repetitions,
+    clippy::multiple_crate_versions,
+    clippy::needless_pass_by_value,
+    clippy::option_if_let_else,
+    clippy::significant_drop_tightening,
+    clippy::similar_names,
+    clippy::wildcard_imports,
+    /* clippy::fallible_impl_from, */
+    /* clippy::significant_drop_in_scrutinee, */
+    /* clippy::must_use_candidate, */
+    /* clippy::unreadable_literal, */
+    /* clippy::use_self, */
+    /* clippy::missing_const_for_fn, */
+    /* clippy::type_repetition_in_bounds, */
+    /* clippy::return_self_not_must_use, */
+    /* clippy::trait_duplication_in_bounds, */
+    /* clippy::inconsistent_struct_constructor, */
+    /* clippy::match_same_arms, */
+    /* clippy::unused_self */
+)]
 
 #[macro_use]
 extern crate log;
