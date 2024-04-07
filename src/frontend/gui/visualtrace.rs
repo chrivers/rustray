48c1cb7666cfb0780ca89cf05243bee31bb6e46b
Christian Iversen <ci@iversenit.dk>
2024-04-07 23:47:19 +0200

    * Simplified visual trace debugger using Camera::world_to_ndc

--- src/frontend/gui/visualtrace.rs
+++ src/frontend/gui/visualtrace.rs
@@ -5,7 +5,20 @@ use egui::{Color32, Pos2, Rect, Rounding, Shape, Stroke};
 
 use crate::debug::tracer::DebugTracer;
 use crate::scene::{BoxScene, RayTracer};
-use crate::{point, Float, Point};
+use crate::{point, Float, Point, Vectorx};
+
+fn color_square(b: Pos2) -> Rect {
+    Rect {
+        min: Pos2 {
+            x: b.x + 20.0,
+            y: b.y - 15.0,
+        },
+        max: Pos2 {
+            x: b.x + 50.0,
+            y: b.y + 15.0,
+        },
+    }
+}
 
 #[must_use]
 pub fn make_shapes<F>(
@@ -32,29 +45,17 @@ where
     for step in dt.steps.borrow().iter() {
         let ray = &step.ray;
 
-        let ax = cam.ndc.pos(cam.projection.pos(cam.model.pos(ray.pos)));
-        let end_pos = step
-            .maxel
-            .map(|m| m.pos)
-            .unwrap_or_else(|| ray.pos + ray.dir);
-        let bx = cam.ndc.pos(cam.projection.pos(cam.model.pos(end_pos)));
-
-        let a = Pos2 {
-            x: ax.x.to_f32().unwrap(),
-            y: ax.y.to_f32().unwrap(),
-        };
-        let mut b = Pos2 {
-            x: bx.x.to_f32().unwrap(),
-            y: bx.y.to_f32().unwrap(),
+        let a: Pos2 = cam.world_to_ndc(ray.pos).point().into();
+        let b: Pos2 = if let Some(maxel) = step.maxel {
+            cam.world_to_ndc(maxel.pos).point().into()
+        } else {
+            let q: Pos2 = cam.world_to_ndc(ray.pos + ray.dir).point().into();
+            let n = (q - a).normalized();
+            a + n * 0.05
         };
 
-        if step.maxel.is_none() {
-            let n = (b - a).normalized();
-            b = a + n * 0.05;
-        }
-
-        let a = to_screen.transform_pos(a);
-        let b = to_screen.transform_pos(b);
+        let a = to_screen.transform_pos(a.into());
+        let b = to_screen.transform_pos(b.into());
         let color = if step.shadow {
             if step.maxel.is_some() {
                 Color32::DARK_RED
@@ -62,7 +63,7 @@ where
                 Color32::DARK_GREEN
             }
         } else {
-            Color32::from_gray(255_i32.saturating_sub(50 * (TRACE_STEPS - ray.lvl) as i32) as u8)
+            Color32::from_gray(255_i32.saturating_sub(50 * ray.lvl as i32) as u8)
         };
 
         if ray.lvl != 0 {
@@ -70,60 +71,41 @@ where
             shapes.push(shape);
         }
 
-        if let Some(mut maxel) = step.maxel {
-            shapes.push(egui::Shape::circle_filled(
-                b,
-                5.0,
-                if step.shadow {
-                    Color32::RED
-                } else {
-                    Color32::BLUE
-                },
-            ));
-
-            let ax = cam.ndc.pos(cam.projection.pos(cam.model.pos(maxel.pos)));
-            let bx = cam.ndc.pos(
-                cam.projection
-                    .pos(cam.model.pos(maxel.pos + maxel.nml() / F::FOUR)),
-            );
-            let a = Pos2 {
-                x: ax.x.to_f32().unwrap(),
-                y: ax.y.to_f32().unwrap(),
-            };
-            let b = Pos2 {
-                x: bx.x.to_f32().unwrap(),
-                y: bx.y.to_f32().unwrap(),
-            };
-            let a = to_screen.transform_pos(a);
-            let b = to_screen.transform_pos(b);
-            let shape = egui::Shape::line_segment([b, a], Stroke::new(1.0, Color32::BLUE));
-            shapes.push(shape);
-
-            if let Some(color) = step.color {
-                let color_sample = Rect {
-                    min: Pos2 {
-                        x: b.x + 20.0,
-                        y: b.y - 15.0,
-                    },
-                    max: Pos2 {
-                        x: b.x + 50.0,
-                        y: b.y + 15.0,
-                    },
-                };
-                shapes.push(egui::Shape::rect_filled(
-                    color_sample,
-                    Rounding::ZERO,
-                    Color32::from(color),
-                ));
-                shapes.push(egui::Shape::rect_stroke(
-                    color_sample,
-                    Rounding::ZERO,
-                    Stroke::new(2.0, Color32::WHITE),
-                ));
-            }
-        } else {
+        let Some(mut maxel) = step.maxel else {
             shapes.push(egui::Shape::circle_filled(b, 3.0, Color32::GREEN));
+            continue;
         };
+
+        shapes.push(egui::Shape::circle_filled(
+            b,
+            5.0,
+            if step.shadow {
+                Color32::RED
+            } else {
+                Color32::BLUE
+            },
+        ));
+
+        let ax = cam.world_to_ndc(maxel.pos);
+        let bx = cam.world_to_ndc(maxel.pos + (maxel.nml() / F::FOUR));
+        let a = to_screen.transform_pos(ax.point().into());
+        let b = to_screen.transform_pos(bx.point().into());
+        let shape = egui::Shape::line_segment([b, a], Stroke::new(1.0, Color32::BLUE));
+        shapes.push(shape);
+
+        if let Some(color) = step.color {
+            let color_sample = color_square(b);
+            shapes.push(egui::Shape::rect_filled(
+                color_sample,
+                Rounding::ZERO,
+                Color32::from(color),
+            ));
+            shapes.push(egui::Shape::rect_stroke(
+                color_sample,
+                Rounding::ZERO,
+                Stroke::new(2.0, Color32::WHITE),
+            ));
+        }
     }
 
     shapes
