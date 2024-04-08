8f3e9b9e806ff7c7fd3199c10e4b79bf141c4b06
Christian Iversen <ci@iversenit.dk>
2024-04-08 23:59:16 +0200

    + Add MaterialLib and MaterialId, which enables building a library of materials to be shared in a scene

---
+++ src/types/matlib.rs
@@ -0,0 +1,40 @@
+use std::collections::HashMap;
+
+use crate::material::Material;
+use crate::types::Float;
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub struct MaterialId(pub u32);
+
+pub struct MaterialLib<F: Float> {
+    pub mats: HashMap<MaterialId, Box<dyn Material<F>>>,
+    idx: u32,
+}
+
+impl<F: Float> MaterialLib<F> {
+    #[must_use]
+    pub fn new() -> Self {
+        Self {
+            mats: HashMap::new(),
+            idx: 0,
+        }
+    }
+
+    #[must_use]
+    pub fn get_name(&self, mat: MaterialId) -> String {
+        format!("material-{}", mat.0)
+    }
+
+    pub fn insert(&mut self, material: Box<dyn Material<F>>) -> MaterialId {
+        let next = MaterialId(self.idx);
+        self.mats.insert(next, material);
+        self.idx += 1;
+        next
+    }
+}
+
+impl<F: Float> Default for MaterialLib<F> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
