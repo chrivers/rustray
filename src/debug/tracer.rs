3d49a07f8fae416b0e2b9c7db4594aef72b00734
Christian Iversen <ci@iversenit.dk>
2024-04-25 21:15:29 +0200

    * Simplified intersection tests using BvhExt::nearest_intersection()

--- src/debug/tracer.rs
+++ src/debug/tracer.rs
@@ -1,12 +1,10 @@
 use std::cell::RefCell;
 use std::fmt::{self, Debug};
 
-use cgmath::MetricSpace;
-
 use crate::light::Lixel;
 use crate::material::Material;
 use crate::scene::{BoxScene, RayTracer};
-use crate::types::{Color, Float, Maxel, Ray};
+use crate::types::{BvhExt, Color, Float, Maxel, Ray};
 
 #[derive(Debug)]
 pub struct Step<'a, F: Float> {
@@ -52,11 +50,6 @@ impl<'a, F: Float> RayTracer<F> for DebugTracer<'a, F> {
         let pos = maxel.pos + maxel.nml() * F::BIAS2;
         let hitray = maxel.ray(pos, lixel.dir);
 
-        let mut best_length = lixel.len2;
-        let mut best_color = None;
-
-        let mut r = hitray.into();
-
         let mut step = Step {
             ray: hitray,
             maxel: None,
@@ -64,29 +57,24 @@ impl<'a, F: Float> RayTracer<F> for DebugTracer<'a, F> {
             color: None,
         };
 
-        #[allow(clippy::significant_drop_in_scrutinee)]
-        for (curobj, _ray) in self
-            .scene
-            .root
-            .bvh
-            .traverse_iter(&mut r, &self.scene.root.geo)
-        {
-            if let Some(mut curhit) = curobj.intersect(&hitray) {
-                let cur_length = maxel.pos.distance2(curhit.pos);
-                if cur_length > F::BIAS2 && cur_length < best_length {
-                    let mat = &self.scene.materials.mats[&maxel.mat];
-                    let color = mat.shadow(&mut curhit, self, lixel);
-                    best_color = Some(color);
-                    best_length = cur_length;
+        let mut len2 = lixel.len2;
 
-                    step.color = best_color;
-                    step.maxel = Some(curhit);
-                }
-            }
-        }
+        if let Some(mut maxel) =
+            self.scene
+                .root
+                .bvh
+                .nearest_intersection(&hitray, &self.scene.root.geo, &mut len2)
+        {
+            let mat = &self.scene.materials.mats[&maxel.mat];
+            step.color = Some(mat.shadow(&mut maxel, self, lixel));
+            step.maxel = Some(maxel);
+        };
+
+        let res = step.color;
+
         self.steps.borrow_mut().push(step);
 
-        best_color
+        res
     }
 
     fn ray_trace(&self, ray: &Ray<F>) -> Option<Color<F>> {
--- src/tracer.rs
+++ src/tracer.rs
@@ -1,13 +1,11 @@
 use std::fmt::{self, Debug};
 
-use cgmath::MetricSpace;
-
 use crate::engine::RenderSpan;
 use crate::light::Lixel;
 use crate::material::Material;
 use crate::scene::{BoxScene, Interactive, RayTracer, SceneObject};
 use crate::sceneobject_impl_body;
-use crate::types::{Camera, Color, Float, Maxel, Point, Ray};
+use crate::types::{BvhExt, Camera, Color, Float, Maxel, Point, Ray};
 
 pub struct Tracer<'a, F: Float> {
     scene: &'a BoxScene<F>,
@@ -70,29 +68,16 @@ impl<'a, F: Float> RayTracer<F> for Tracer<'a, F> {
         let pos = maxel.pos + maxel.nml() * F::BIAS2;
         let hitray = maxel.ray(pos, lixel.dir);
 
-        let mut best_length = lixel.len2;
-        let mut best_color = None;
+        let mut len2 = lixel.len2;
 
-        let mut r = hitray.into();
-
-        #[allow(clippy::significant_drop_in_scrutinee)]
-        for (curobj, _ray) in self
-            .scene
+        self.scene
             .root
             .bvh
-            .traverse_iter(&mut r, &self.scene.root.geo)
-        {
-            if let Some(mut curhit) = curobj.intersect(&hitray) {
-                let cur_length = maxel.pos.distance2(curhit.pos);
-                if cur_length > F::BIAS2 && cur_length < best_length {
-                    let mat = &self.scene.materials.mats[&curhit.mat];
-                    let color = mat.shadow(&mut curhit, self, lixel);
-                    best_color = Some(color);
-                    best_length = cur_length;
-                }
-            }
-        }
-        best_color
+            .nearest_intersection(&hitray, &self.scene.root.geo, &mut len2)
+            .map(|mut maxel| {
+                let mat = &self.scene.materials.mats[&maxel.mat];
+                mat.shadow(&mut maxel, self, lixel)
+            })
     }
 
     fn ray_trace(&self, ray: &Ray<F>) -> Option<Color<F>> {
